546acf9b05cac084ce89f13f6eef4e3364ede5ca
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-18 16:37:27 -0300

    Cadastrar um novo lançamento

--- src/main/java/br/com/algamoneyapi3/resource/LancamentoResource.java
+++ src/main/java/br/com/algamoneyapi3/resource/LancamentoResource.java
@@ -3,13 +3,20 @@ package br.com.algamoneyapi3.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.algamoneyapi3.event.RecursoCriadoEvent;
 import br.com.algamoneyapi3.model.Lancamento;
 import br.com.algamoneyapi3.repository.LancamentoRepository;
 
@@ -19,6 +26,9 @@ public class LancamentoResource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Lancamento> listar() {
@@ -30,5 +40,12 @@ public class LancamentoResource {
 		Optional<Lancamento> lancamento = lancamentoRepository.findById(codigo);
 		return lancamento.isPresent() ?
 				ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 }
