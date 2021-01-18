fcfb50d1d990d295b8703d8ce528cd7824724b47
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-18 13:39:44 -0300

    Cadastrando nova categoria com POST-Salvar a primeira categoria

--- src/main/java/br/com/algamoneyapi3/resource/CategoriaResource.java
+++ src/main/java/br/com/algamoneyapi3/resource/CategoriaResource.java
@@ -2,11 +2,19 @@ package br.com.algamoneyapi3.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.algamoneyapi3.event.RecursoCriadoEvent;
 import br.com.algamoneyapi3.model.Categoria;
 import br.com.algamoneyapi3.repository.CategoriaRepository;
 
@@ -17,8 +25,18 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 }
