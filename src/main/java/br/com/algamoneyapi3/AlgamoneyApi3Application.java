46c04732d72f66ca1f74632415cbabaf83c2d325
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-18 12:06:43 -0300

    Projeto criado

---
+++ src/main/java/br/com/algamoneyapi3/AlgamoneyApi3Application.java
@@ -0,0 +1,13 @@
+package br.com.algamoneyapi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApi3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApi3Application.class, args);
+	}
+
+}
