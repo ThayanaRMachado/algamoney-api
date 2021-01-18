0e367e01aed99fd5c74fb6674432e88799149ac9
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-18 15:52:55 -0300

    Atualizar propriedade ativo

--- src/main/java/br/com/algamoneyapi3/resource/PessoaResource.java
+++ src/main/java/br/com/algamoneyapi3/resource/PessoaResource.java
@@ -69,4 +69,10 @@ public class PessoaResource {
 		return ResponseEntity.ok(pessoaSalva);
 	}
 	
+	@PutMapping("/{codigo}/ativo")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
+	
 }
--- src/main/java/br/com/algamoneyapi3/service/PessoaService.java
+++ src/main/java/br/com/algamoneyapi3/service/PessoaService.java
@@ -1,5 +1,7 @@
 package br.com.algamoneyapi3.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -20,5 +22,17 @@ public class PessoaService {
 		
 		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
 		return pessoaRepository.save(pessoaSalva);
+	}
+	
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+	}
+
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		
+		return pessoa.orElseThrow(() -> new EmptyResultDataAccessException(1));
 	}
 }
