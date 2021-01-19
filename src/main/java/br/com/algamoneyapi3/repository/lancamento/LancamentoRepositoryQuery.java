827c402f7d6b9ba59fb3bc00b8e1be8ccf5c19a8
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-19 16:35:11 -0300

    Implementando a paginação de lançamentos

--- src/main/java/br/com/algamoneyapi3/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/br/com/algamoneyapi3/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package br.com.algamoneyapi3.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.com.algamoneyapi3.model.Lancamento;
 import br.com.algamoneyapi3.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
 }
