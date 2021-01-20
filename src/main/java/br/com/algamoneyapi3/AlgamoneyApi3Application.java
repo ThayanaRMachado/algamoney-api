b089086d67476e50930c686e0e4b0cffd44f8a34
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-20 15:10:08 -0300

    Criando filtro para CORS

--- src/main/java/br/com/algamoneyapi3/AlgamoneyApi3Application.java
+++ src/main/java/br/com/algamoneyapi3/AlgamoneyApi3Application.java
@@ -2,6 +2,9 @@ package br.com.algamoneyapi3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AlgamoneyApi3Application {
@@ -10,4 +13,13 @@ public class AlgamoneyApi3Application {
 		SpringApplication.run(AlgamoneyApi3Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8000");
+			}
+		};
+	}
 }
