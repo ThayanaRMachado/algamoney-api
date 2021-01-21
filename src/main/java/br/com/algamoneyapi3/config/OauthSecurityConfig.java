e45eab9e597a4def0f57b5147989882d169b276e
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-21 16:38:03 -0300

    Alternando OAuth 2 e Basic Security com profiles

--- src/main/java/br/com/algamoneyapi3/config/OauthSecurityConfig.java
+++ src/main/java/br/com/algamoneyapi3/config/OauthSecurityConfig.java
@@ -1,52 +1,27 @@
 package br.com.algamoneyapi3.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Profile("oauth-security")
 @Configuration
 @EnableWebSecurity
-public class OauthSecurityConfig extends WebSecurityConfigurerAdapter{
+public class OauthSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	
-	  @Bean  
-	  @Override protected AuthenticationManager authenticationManager() throws
-	  Exception { return super.authenticationManager(); }
-	 
-	
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
+	@Bean
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	public AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest().authenticated()
-			.and()
-			.httpBasic()
-			.and()
-			.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.csrf().disable();
 	}
 }
