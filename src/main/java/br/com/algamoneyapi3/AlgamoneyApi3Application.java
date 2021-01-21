b766bff7e733f4f2814286d1122ce09c80ed3563
ThayanaRMachado <thayanarm@hotmail.com>
2021-01-21 12:06:57 -0300

    Profiles do Spring

--- src/main/java/br/com/algamoneyapi3/AlgamoneyApi3Application.java
+++ src/main/java/br/com/algamoneyapi3/AlgamoneyApi3Application.java
@@ -2,11 +2,15 @@ package br.com.algamoneyapi3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import br.com.algamoneyapi3.config.property.AlgamoneyApiProperty;
+
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApi3Application {
 
 	public static void main(String[] args) {
